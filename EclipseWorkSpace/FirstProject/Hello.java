dc03bee894f5d949b37ec11d8ada894d5b9aace1
tatertatitotz <chock.tatiana@outlook.com>
2026-01-28 11:29:09 -0700

    testing to see if changes work
    test to see if environment is ready for class

--- EclipseWorkSpace/FirstProject/Hello.java
+++ EclipseWorkSpace/FirstProject/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Print hello
-		System.out.print("Hello");
+		System.out.print("Testing to see if github works!");
 
 	}
 
