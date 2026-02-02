00dd01f8ec063d84bcfae9e03b9d54764cb93e6c
tatertatitotz <chock.tatiana@outlook.com>
2026-02-02 13:47:26 -0700

    added comments for algorithm clarity

--- EclipseWorkSpace/CS1050M01/GEM01GradeCalculations.java
+++ EclipseWorkSpace/CS1050M01/GEM01GradeCalculations.java
@@ -24,12 +24,19 @@ public class GEM01GradeCalculations
 
 		// algorithm
 		// weights * grades of the same name
+
+		// calculates weighted class participation grade
 		double totalPart = CLASS_PART * classPart;
+		// calculates weighted guided exploration grade
 		double totalGuided = GUIDED_EX * guidedEx;
+		// calculates weighted quiz grade
 		double totalQuizzes = QUIZZES * quizzes;
+		// calculates weighted project grade
 		double totalProjects = PROJECTS * projects;
+		// calculates weighted final demonstration grade
 		double totalFinalDem = FINAL_DEMO * finalDemo;
 
+		// adds them all up for grade
 		// final algorithm step
 		double grade = totalPart + totalGuided + totalQuizzes + totalProjects + totalFinalDem;
 
