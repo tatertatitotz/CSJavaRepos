cdbc5df7ecefaae4e8c8964d45dfee3f178664d5
tatertatitotz <chock.tatiana@outlook.com>
2026-02-02 13:31:01 -0700

    added the name date
    added end curly brace comments too

--- EclipseWorkSpace/CS1050M01/GEM01GradeCalculations.java
+++ EclipseWorkSpace/CS1050M01/GEM01GradeCalculations.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
-
-/**
- * 
+/*
+ *  Name: [TAT CHOCK]
+ *  Class: CS1050 (M/W) 2/2/2026
+ *  Description: Guided Exploration 01      
+ *  The program will calculate a final grade for this class based on the category weights 
  */
 public class GEM01GradeCalculations
 {
@@ -15,6 +14,6 @@ public class GEM01GradeCalculations
 	{
 		// TODO Auto-generated method stub
 
-	}
+	}// end of main
 
-}
+}// end of class
