2f6b67854c59c94b5b46f7513298a9eace4091b2
tatertatitotz <chock.tatiana@outlook.com>
2026-02-02 13:25:41 -0700

    Created GE01GradeCalculations

---
+++ EclipseWorkSpace/CS1050M01/GEM01GradeCalculations.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class GEM01GradeCalculations
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
