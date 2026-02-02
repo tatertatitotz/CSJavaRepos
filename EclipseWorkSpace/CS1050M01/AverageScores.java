4265c51cf87eb6c1b65a92d8557acb3b990b4f92
tatertatitotz <chock.tatiana@outlook.com>
2026-02-02 13:03:02 -0700

    added AverageScore.java

---
+++ EclipseWorkSpace/CS1050M01/AverageScores.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class AverageScores
+{
+	public static void main(String[] args)
+	{
+		int score1 = 80;
+		int score2 = 97;
+		int score3 = 85;
+		int total = score1 + score2 + score3;
+		double average = total / 3;
+		System.out.println("Total: " + total);
+		System.out.println("Average " + average);
+	}// end of main
+}// end of class
