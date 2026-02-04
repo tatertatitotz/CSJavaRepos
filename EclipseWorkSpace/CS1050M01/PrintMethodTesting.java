ff564818849bceceaf2fa7ed97efc3f6911ae4ad
tatertatitotz <chock.tatiana@outlook.com>
2026-02-04 14:14:19 -0700

    Update PrintMethodTesting.java
    removed unnecessary spacing and comments.

--- EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
+++ EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
@@ -1,19 +1,11 @@
-/**
- * 
- */
 
-/**
- * 
- */
 public class PrintMethodTesting
 {
 
 	/**
-	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 
 	}
 
