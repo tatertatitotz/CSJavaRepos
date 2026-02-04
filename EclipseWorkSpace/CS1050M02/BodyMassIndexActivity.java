84f29742b98704c326feabf5b9909b2996fec812
tatertatitotz <chock.tatiana@outlook.com>
2026-02-04 13:38:12 -0700

    added the ability to type in weight and height
    allows the user to input height and weight using their keyboard

--- EclipseWorkSpace/CS1050M02/BodyMassIndexActivity.java
+++ EclipseWorkSpace/CS1050M02/BodyMassIndexActivity.java
@@ -28,11 +28,11 @@ public class BodyMassIndexActivity
 
 		// Get the user's weight.
 		System.out.print("Enter your weight, in pounds: ");
-		// ADD CODE to input next double for weight
+		weight = keyboardInput.nextDouble();
 
 		// Get the user's height.
 		System.out.print("Enter your height, in inches: ");
-		// ADD CODE input next double for height
+		height = keyboardInput.nextDouble();
 
 		// Calculate the user's body mass index.
 		double bmi = weight * BMI_US_FACTOR / (height * height);
