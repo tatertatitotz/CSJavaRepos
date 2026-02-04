90d2766a332b2d153a0d2387c2b87c9b1f19cd00
tatertatitotz <chock.tatiana@outlook.com>
2026-02-04 14:26:40 -0700

    Updated PMT.java added comment
    begin method testing

--- EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
+++ EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
@@ -3,9 +3,11 @@ public class PrintMethodTesting
 {
 
 	/**
+	 * This class is just about testing print() and its variations
 	 */
 	public static void main(String[] args)
 	{
+		System.out.print("hello");
 
 	}
 
