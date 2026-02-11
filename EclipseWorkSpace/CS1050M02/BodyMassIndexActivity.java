79c9a85b4d00c0d4c353ff205592dd8fc76ef190
tatertatitotz <chock.tatiana@outlook.com>
2026-02-11 13:38:35 -0700

    Changed height * height to Math.pow
    height^2

--- EclipseWorkSpace/CS1050M02/BodyMassIndexActivity.java
+++ EclipseWorkSpace/CS1050M02/BodyMassIndexActivity.java
@@ -35,6 +35,7 @@ public class BodyMassIndexActivity
 
 		// Calculate the user's body mass index.
 		double bmi = weight * BMI_US_FACTOR / (height * height);
+		double bmi = weight * BMI_US_FACTOR / (Math.pow(height, 2));
 
 		// Display the user's BMI.
 		System.out.printf("Your body mass index (BMI) is %f", bmi);
