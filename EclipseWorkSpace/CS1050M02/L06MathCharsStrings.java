38c0cf2830e0e6e2632714c1c124d2278ebf3e63
tatertatitotz <chock.tatiana@outlook.com>
2026-02-12 19:50:49 -0700

    Update L06 commented out Char
    makes it easier to read the console

--- EclipseWorkSpace/CS1050M02/L06MathCharsStrings.java
+++ EclipseWorkSpace/CS1050M02/L06MathCharsStrings.java
@@ -42,26 +42,33 @@ public class L06MathCharsStrings
 		// -------- practice -------------------
 
 		// keyboardInput is the identifier for running the Scanner method
-		Scanner keyboardInput = new Scanner(System.in);
+//		Scanner keyboardInput = new Scanner(System.in);
+//
+//		// console display for user
+//		System.out.print("What is your first name?: ");
+//		// declares and initializes name1 as the input given from the Scanner
+//		String name1 = keyboardInput.next();
+//
+//		// console display for user
+//		System.out.print("What is your last name?: ");
+//		// declares and initializes lastIntial
+//		char lastInitial = keyboardInput.next().charAt(0);
+//
+//		// Displays a Hello message with all given inputs
+//		System.out.print("Hello " + name1 + " " + lastInitial + "!");
+//
+//		keyboardInput.close();
 
-		// console display for user
-		System.out.print("What is your first name?: ");
-		// declares and initializes name1 as the input given from the Scanner
-		String name1 = keyboardInput.next();
 
-		// console display for user
-		System.out.print("What is your last name?: ");
-		// declares and initializes lastIntial
-		char lastInitial = keyboardInput.next().charAt(0);
 
-		// Displays a Hello message with all given inputs
-		System.out.print("Hello " + name1 + " " + lastInitial + "!");
 
 		keyboardInput.close();
+//		lastName
+//		noun
+//		verb
+//		adjective
+//		place
 
-		// -------- Mad Lib ---------------------
-
-		Scanner madLibInput = new Scanner(System.in);
 
 	}
 
