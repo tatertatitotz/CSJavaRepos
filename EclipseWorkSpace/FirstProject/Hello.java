74dd2b0cd6461f67802b4f2131fd2a087bcc3d26
tatertatitotz <chock.tatiana@outlook.com>
2026-02-18 18:40:04 -0700

    Testing switch

--- EclipseWorkSpace/FirstProject/Hello.java
+++ EclipseWorkSpace/FirstProject/Hello.java
@@ -16,8 +16,46 @@ public class Hello
 				System.out.print("Integer = 4");
 			}
 
+		} // end if
+
+		int number = 5;
+
+		switch (number)
+		{
+
+		case 1:
+		{
+			System.out.print("This sure is a 1");
+			break;
+		}
+		case 2:
+		{
+			System.out.print("2 for you");
+			break;
+		}
+		case 3:
+		{
+			System.out.print("3 for me");
+			break;
+		}
+		case 4:
+		{
+			System.out.print("4 for more");
+			break;
+		}
+		case 5:
+		{
+			System.out.print("5!");
+			break;
+		}
+		default:
+		{
+			System.out.print("This sure is a 1");
+			break;
 		}
 
-	}
+		}
 
-}
+	}// end main
+
+}// end class
