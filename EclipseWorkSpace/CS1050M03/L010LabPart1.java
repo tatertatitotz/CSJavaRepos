6eac2b532a94c17df5080680e1be15571b16631a
tatertatitotz <chock.tatiana@outlook.com>
2026-02-23 13:45:12 -0700

    While loop when exponent is not pos

--- EclipseWorkSpace/CS1050M03/L010LabPart1.java
+++ EclipseWorkSpace/CS1050M03/L010LabPart1.java
@@ -7,8 +7,15 @@ public class L010LabPart1
 	{
 		Scanner keyboardInput = new Scanner(System.in);
 		int exponent;
+		while (exponent <= 0);
+		{
+			System.out.println("Error: Exponent inputted is negative or zero.");
 			System.out.print("Enter a positive number for an exponent: ");
 			exponent = keyboardInput.nextInt();
+		}
+
+		System.out.printf("The exponent is: %d", exponent);
+
 	}
 
 }
