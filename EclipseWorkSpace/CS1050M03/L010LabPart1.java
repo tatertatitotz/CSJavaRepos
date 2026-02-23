f5e31cdb0bb493e60d91203a2654573522d8c1bf
tatertatitotz <chock.tatiana@outlook.com>
2026-02-23 13:44:17 -0700

    Declared Scanner and exponent

--- EclipseWorkSpace/CS1050M03/L010LabPart1.java
+++ EclipseWorkSpace/CS1050M03/L010LabPart1.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class L010LabPart1
 {
+
 	public static void main(String[] args)
 	{
+		Scanner keyboardInput = new Scanner(System.in);
+		int exponent;
 	}
 
 }
