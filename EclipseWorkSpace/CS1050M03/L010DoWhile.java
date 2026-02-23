ea8349f156c6c426a99d39dfb061a3ba737a2ac6
tatertatitotz <chock.tatiana@outlook.com>
2026-02-23 14:28:10 -0700

    Added crucial aspects to do while

--- EclipseWorkSpace/CS1050M03/L010DoWhile.java
+++ EclipseWorkSpace/CS1050M03/L010DoWhile.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class L010DoWhile
 {
 
 	public static void main(String[] args)
 	{
+		// creates a scanner called keyboardInput
+		Scanner keyboardInput = new Scanner(System.in);
+		// declares exponent
+		int exponent;
+		do
+		{
+			{
+				System.out.println("Error: Exponent inputted is negative or zero.");
+			}
+		} while (exponent <= 0);
+		System.out.printf("The exponent is: %d", exponent);
 	}
 
 }
