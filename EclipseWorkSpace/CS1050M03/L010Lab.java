bc61f8e7590d4df420d197361a89dc5752b7ca77
tatertatitotz <chock.tatiana@outlook.com>
2026-02-23 14:27:35 -0700

    Changed the name of L010
    removed the Part1

--- EclipseWorkSpace/CS1050M03/L010Lab.java
+++ EclipseWorkSpace/CS1050M03/L010Lab.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
-public class L010LabPart1
+public class L010Lab
 {
 
 	public static void main(String[] args)
 	{
+		// creates a scanner called keyboardInput
 		Scanner keyboardInput = new Scanner(System.in);
+		// declares exponent
 		int exponent;
 
-		do
-		{
-			System.out.print("Enter a positive number for an exponent: ");
-			exponent = keyboardInput.nextInt();
-		}
+		// Prompt for input
+		System.out.print("Enter a positive number for an exponent: ");
+		// initializes exponent
+		exponent = keyboardInput.nextInt();
 
-		while (exponent <= 0);
+		while (exponent <= 0)
 		{
 			System.out.println("Error: Exponent inputted is negative or zero.");
 			System.out.print("Enter a positive number for an exponent: ");
