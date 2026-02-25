0f2da0cfcd0e29399593668ba225511509c27997
tatertatitotz <chock.tatiana@outlook.com>
2026-02-25 11:32:07 -0700

    declared and initialized dice

--- EclipseWorkSpace/CS1050M02/DiceGame.java
+++ EclipseWorkSpace/CS1050M02/DiceGame.java
@@ -4,7 +4,13 @@ public class DiceGame
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		// declared dice1 and dice2
+		int dice1;
+		int dice2;
+
+		// initialized dice1 and dice 2 as Math.random which has a range of 1 <= x < 7
+		dice1 = (int) (Math.random() * 6 + 1);
+		dice2 = (int) (Math.random() * 6 + 1);
 
 	}
 
