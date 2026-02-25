0211cbaae9f04619eed7a63e5f9972e832557d7c
tatertatitotz <chock.tatiana@outlook.com>
2026-02-25 11:53:58 -0700

    testing order of operations

--- EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
+++ EclipseWorkSpace/CS1050M01/PrintMethodTesting.java
@@ -15,6 +15,13 @@ public class PrintMethodTesting
 		System.out.println("Hello " + firstName);
 
 		int compileTimeError = 4;
+
+		int num1 = 10;
+		int num2 = 2;
+
+		charAsciiValue = (num1 / (num2 + num2));
+
+		System.out.print(charAsciiValue);
 	}
 
 }
