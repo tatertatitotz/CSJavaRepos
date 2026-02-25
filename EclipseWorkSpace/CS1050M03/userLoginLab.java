0a703e396e66397e42effd09fabfe88854c069a1
tatertatitotz <chock.tatiana@outlook.com>
2026-02-25 13:13:32 -0700

    declared constants and variables, created scanner

--- EclipseWorkSpace/CS1050M03/userLoginLab.java
+++ EclipseWorkSpace/CS1050M03/userLoginLab.java
@@ -1,7 +1,22 @@
+
+//added scanner for user input
+import java.util.Scanner;
+
 public class userLoginLab
 {
+
 	public static void main(String[] args)
 	{
+		// created scanner keyboardInput
+		Scanner keyboardInput = new Scanner(System.in);
+
+		// constants for testing
+		final String USERNAME_ACTUAL = "username123";
+		final String PASSWORD_ACTUAL = "password123";
+
+		// declared space for inputs
+		String inputedUsername;
+		String inputedPassword;
 	}
 
 }
