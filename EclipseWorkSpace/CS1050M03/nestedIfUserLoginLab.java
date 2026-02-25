52f348d9356a55745b463d4093c5a8db8339b4da
tatertatitotz <chock.tatiana@outlook.com>
2026-02-25 13:35:51 -0700

    Created nestedIfUserLoginLab.java

---
+++ EclipseWorkSpace/CS1050M03/nestedIfUserLoginLab.java
@@ -0,0 +1,41 @@
+
+//added scanner for user input
+import java.util.Scanner;
+
+public class nestedIfUserLoginLab
+{
+
+	public static void main(String[] args)
+	{
+		// created scanner keyboardInput
+		Scanner keyboardInput = new Scanner(System.in);
+
+		// constants for testing
+		final String USERNAME_ACTUAL = "username123";
+		final String PASSWORD_ACTUAL = "password123";
+
+		// declared space for inputs
+		String inputedUsername;
+		String inputedPassword;
+
+		// initialized inputedUsername
+		System.out.print("Input username: ");
+		inputedUsername = keyboardInput.next();
+
+		// initialized inputedPassword
+		System.out.print("Input password: ");
+		inputedPassword = keyboardInput.next();
+
+		// if user name is correct
+		if (USERNAME_ACTUAL.equals(inputedUsername))
+		{
+		}
+		// if user name is incorrect
+		else
+		{
+		}
+
+		keyboardInput.close();
+	}
+
+}
