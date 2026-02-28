d7fdaa60c20fbfcf7b4e6a0f68f5cb67b3bfcd00
tatertatitotz <chock.tatiana@outlook.com>
2026-02-27 21:36:31 -0700

    Put everything in a do while for loop

--- EclipseWorkSpace/CS1050M03/nestedIfUserLoginLab.java
+++ EclipseWorkSpace/CS1050M03/nestedIfUserLoginLab.java
@@ -18,45 +18,61 @@ public class nestedIfUserLoginLab
 		String inputedUsername;
 		String inputedPassword;
 
-		// initialized inputedUsername
-		System.out.print("Input username: ");
-		inputedUsername = keyboardInput.next();
-
-		// initialized inputedPassword
-		System.out.print("Input password: ");
-		inputedPassword = keyboardInput.next();
-
-		// if user name is correct
-		if (USERNAME_ACTUAL.equals(inputedUsername))
+		// declared a boolean to fit in the scope of while
+		boolean loggedIn = true;
+		do
 		{
-			// if both user name and password are correct
-			if (PASSWORD_ACTUAL.equals(inputedPassword))
-			{
-				System.out.print("Login successful!");
-			}
-			// if ONLY the USER NAME is correct
-			else
-			{
-				System.out.println("Incorrect password");
-			}
-		}
-		// if user name is incorrect
-		else
-		{
-			// if ONLY the password is correct
-			if (PASSWORD_ACTUAL.equals(inputedPassword))
-			{
-				System.out.println("Incorrect username");
 
-			}
-			// if BOTH are incorrect
-			else
+			for (int attempts = 0; attempts <= 3; ++attempts)
 			{
-				System.out.print("Both username and password are incorrect. Login failed.");
+				// initialized inputedUsername
+				System.out.print("Input username: ");
+				inputedUsername = keyboardInput.next();
+
+				// initialized inputedPassword
+				System.out.print("Input password: ");
+				inputedPassword = keyboardInput.next();
+
+				// assumes the log in is true
+				loggedIn = true;
+
+				// if user name is correct
+				if (USERNAME_ACTUAL.equals(inputedUsername))
+				{
+					// if both user name and password are correct
+					if (PASSWORD_ACTUAL.equals(inputedPassword))
+					{
+						System.out.print("Login successful!");
+					}
+					// if ONLY the USER NAME is correct
+					else
+					{
+						System.out.println("Incorrect password");
+						loggedIn = false;
+					}
+				}
+				// if user name is incorrect
+				else
+				{
+					// if ONLY the password is correct
+					if (PASSWORD_ACTUAL.equals(inputedPassword))
+					{
+						System.out.println("Incorrect username");
+					}
+					// if BOTH are incorrect
+					else
+					{
+						System.out.println("Both username and password are incorrect. Login failed.");
+					}
+					loggedIn = false;
+				}
 			}
-		}
+		} // end of do
+		while (loggedIn == false);
+
+		System.out.print("3 Attempts.");
 
 		keyboardInput.close();
-	}
+	}// end of main
 
-}
+}// end of class
