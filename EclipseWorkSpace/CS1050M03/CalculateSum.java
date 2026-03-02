51eb13c0787822a80b61654e86d5fb431de6cc68
tatertatitotz <chock.tatiana@outlook.com>
2026-03-02 14:40:55 -0700

    Testing for loop functionality

--- EclipseWorkSpace/CS1050M03/CalculateSum.java
+++ EclipseWorkSpace/CS1050M03/CalculateSum.java
@@ -5,5 +5,12 @@ public class CalculateSum
 
 	public static void main(String[] args)
 	{
+		int sum = 0;
+		int max = 6;
 	}// end of main
+		for (int add = 0; add <= max; ++add)
+		{
+			sum = sum + add;
+			System.out.println(sum);
+		}
 }// end of class
