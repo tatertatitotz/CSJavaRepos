f1f53d9a4c8f8e985b95e8bd2ec334b4eadcc064
tatertatitotz <chock.tatiana@outlook.com>
2026-03-04 13:07:12 -0700

    while loop for invalid inputs.

--- EclipseWorkSpace/CS1050M02/RockPaperScissorsGame.java
+++ EclipseWorkSpace/CS1050M02/RockPaperScissorsGame.java
@@ -25,6 +25,20 @@ public class RockPaperScissorsGame
 		// allows the player to input a string. It is implicitly casting a String into a
 		// char
 		player = Character.toUpperCase(keyboardInput.next().charAt(0));
-	}
 
-}
+
+		// this is a boolean flag that assumes that the input by player is valid
+
+		// if the input from player is NOT Rock Paper or Scissors it makes you input it
+		// again
+		while (player != 'R' || player != 'P' || player != 'S')
+		{
+			// console output when the input is not valid
+			System.out.print("Improper input. Please try again: ");
+			// user end console prompt
+			System.out.println("Type one of the following: \n R: Rock \n P: Paper \n S: Scissors");
+			System.out.print("Input it here: ");
+			// allows the player to input a string. It is implicitly casting a String into a
+			// char
+			player = Character.toUpperCase(keyboardInput.next().charAt(0));
+		} // end of while
