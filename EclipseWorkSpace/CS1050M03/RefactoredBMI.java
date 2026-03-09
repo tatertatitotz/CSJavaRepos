819737120b87dd2b839837b8974f5d4697a05901
tatertatitotz <chock.tatiana@outlook.com>
2026-03-09 14:17:58 -0600

    Added inputted summary

--- EclipseWorkSpace/CS1050M03/RefactoredBMI.java
+++ EclipseWorkSpace/CS1050M03/RefactoredBMI.java
@@ -17,6 +17,12 @@ public class RefactoredBMI
 
 		Scanner keyboardInput = new Scanner(System.in);
 
+		// Tell the user what the program will do.
+		System.out.println(
+				"This program will calculate your body mass index, or BMI using the imperial measurement system (pounds and inches).");
+		System.out.println("There are 4 BMI Categories: " + "\n Obese: BMI = +40 " + "\n Overweight: BMI >= 25 "
+				+ "\n Normal: BMI >= 18.5 " + "\n Underweight: BMI < 18.5.");
+		System.out.println("The BMI formula used is: weight * 703 / height^2.");
 		// Get the user's weight.
 		System.out.print("Enter your weight, in pounds: ");
 		weight = keyboardInput.nextDouble();
