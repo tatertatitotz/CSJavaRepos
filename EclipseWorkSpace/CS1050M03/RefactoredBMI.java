a657e0ea5a6fd45780cb29dbfbc975d7c5e10ad4
tatertatitotz <chock.tatiana@outlook.com>
2026-03-10 12:01:31 -0600

    added printSummary Method

--- EclipseWorkSpace/CS1050M03/RefactoredBMI.java
+++ EclipseWorkSpace/CS1050M03/RefactoredBMI.java
@@ -5,14 +5,7 @@ public class RefactoredBMI
 
 	public static void main(String[] args)
 	{
-
-		// Tell the user what the program will do.
-		System.out.println(
-				"This program will calculate your body mass index, or BMI using the imperial measurement system (pounds and inches).");
-		System.out.println("There are 4 BMI Categories: " + "\n Obese: BMI = +40 " + "\n Overweight: BMI >= 25 "
-				+ "\n Normal: BMI >= 18.5 " + "\n Underweight: BMI < 18.5.");
-		System.out.println("The BMI formula used is: weight * 703 / height^2.");
-
+		printSummary();
 		// new scanner because new method
 		Scanner keyboardInput = new Scanner(System.in);
 		// Variables
@@ -40,6 +33,17 @@ public class RefactoredBMI
 		} while (continueYN == true);
 
 	}// end of main
+
+	public static void printSummary()
+	{
+		// Tell the user what the program will do.
+		System.out.println(
+				"This program will calculate your body mass index, or BMI using the imperial measurement system (pounds and inches).");
+		System.out.println("There are 4 BMI Categories: " + "\n Obese: BMI = +40 " + "\n Overweight: BMI >= 25 "
+				+ "\n Normal: BMI >= 18.5 " + "\n Underweight: BMI < 18.5.");
+		System.out.println("The BMI formula used is: weight * 703 / height^2.");
+
+	}
 
 	// checks if the inputed weight is valid
 	public static double checkWeight(Scanner inputAttempt)
