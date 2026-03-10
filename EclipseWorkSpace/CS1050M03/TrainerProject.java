80d809b8241b0bcea392b78844cbb5e360962aec
tatertatitotz <chock.tatiana@outlook.com>
2026-03-10 12:09:21 -0600

    Created printSummary method
    Every time the method printSummary runs, it prints a summary of what the program is going to do for the user.

--- EclipseWorkSpace/CS1050M03/TrainerProject.java
+++ EclipseWorkSpace/CS1050M03/TrainerProject.java
@@ -3,6 +3,19 @@ public class TrainerProject
 
 	public static void main(String[] args)
 	{
+		// prints program summary
+		printSummary();
 
 	}
+
+	public static void printSummary()
+	{
+		// Tell the user what the program will do.
+		System.out.println(
+				"This program will calculate your body mass index, or BMI using the imperial measurement system (pounds and inches).");
+		System.out.println("There are 4 BMI Categories: " + "\n Obese: BMI = +40 " + "\n Overweight: BMI >= 25 "
+				+ "\n Normal: BMI >= 18.5 " + "\n Underweight: BMI < 18.5.");
+		System.out.println("The BMI formula used is: weight * 703 / height^2.");
+	}
+
 }
