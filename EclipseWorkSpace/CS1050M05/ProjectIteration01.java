a36b79c24df3dd037255a2ac62402d2df533d10c
tatertatitotz <chock.tatiana@outlook.com>
2026-04-08 13:41:51 -0600

    methods for array length

--- EclipseWorkSpace/CS1050M05/ProjectIteration01.java
+++ EclipseWorkSpace/CS1050M05/ProjectIteration01.java
@@ -11,6 +11,33 @@ public class ProjectIteration01
 		double[] heartRatesMain;
 		double[] bmiMain;
 
-}
+		int numberOfAthletes = howManyAthletes(inputMain);
+
 	}// end main
+
+	public static boolean getValidNumber(double checkNumber)
+	{
+		boolean positiveNumber = true;
+
+		if (checkNumber <= 0)
+		{
+			System.out.println("Error: Value must be greater than 0.");
+			positiveNumber = false;
+		}
+
+		return positiveNumber;
+	}
+
+	public static int howManyAthletes(Scanner inputNumber)
+	{
+		int athletes;
+
+		do
+		{
+			System.out.print("Enter the number of athletes on the team: ");
+			athletes = inputNumber.nextInt();
+		} while (getValidNumber(athletes) == false);
+
+		return athletes;
+	}
 }// end class
