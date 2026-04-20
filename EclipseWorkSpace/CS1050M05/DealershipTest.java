41d1733fc7cbdf5191b03bafd7a9e82cd2c1fa70
tatertatitotz <chock.tatiana@outlook.com>
2026-04-20 13:44:01 -0600

    testing getModel finished

--- EclipseWorkSpace/CS1050M05/DealershipTest.java
+++ EclipseWorkSpace/CS1050M05/DealershipTest.java
@@ -5,6 +5,7 @@ public class DealershipTest
 	public static void main(String[] args)
 	{
 		Car car1 = new Car("Ford", "Mustang", 32800);
+		System.out.println(car1.getModel());
 	}// end main
 
 }// end Dealership Test
@@ -21,4 +22,10 @@ class Car
 		this.make = make;
 		this.price = price;
 	}
+
+	public String getModel()
+	{
+		return model;
+	}
+
 }// end Car
