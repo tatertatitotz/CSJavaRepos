a741536b0a91cdbf6114d5bb15293e96aa7a0620
tatertatitotz <chock.tatiana@outlook.com>
2026-04-20 13:43:44 -0600

    car constructor

--- EclipseWorkSpace/CS1050M05/DealershipTest.java
+++ EclipseWorkSpace/CS1050M05/DealershipTest.java
@@ -4,6 +4,21 @@ public class DealershipTest
 
 	public static void main(String[] args)
 	{
+		Car car1 = new Car("Ford", "Mustang", 32800);
 	}// end main
 
 }// end Dealership Test
+
+class Car
+{
+	private String model;
+	private String make;
+	private double price;
+
+	public Car(String model, String make, double price)
+	{
+		this.model = model;
+		this.make = make;
+		this.price = price;
+	}
+}// end Car
