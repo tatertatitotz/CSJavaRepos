48b8fddb78f8efc0b65993cf05978e201bc6309d
tatertatitotz <chock.tatiana@outlook.com>
2026-04-22 14:10:43 -0600

    Created Athlete class

--- EclipseWorkSpace/CS1050M05/ProjectIteration02.java
+++ EclipseWorkSpace/CS1050M05/ProjectIteration02.java
@@ -41,31 +41,20 @@ public class ProjectIteration02
 		heartRatesMain = new double[numberOfAthletes];
 		bmiMain = new double[numberOfAthletes];
 
-		// allows user to enter custom athlete data
-		enterAthleteData(namesMain, bmiMain, heartRatesMain, inputMain);
-
 		// console separation
 		System.out.println("========== Athlete Summary==========");
-		// console output with all athlete data that was inputed
-		displayAthleteBMI(namesMain, bmiMain, heartRatesMain);
 
 		// console separation
 		System.out.println("========== BMI Analysis ==========");
-		// makes a list of people above/below normal bmi category (if none it displays
-		// that too)
-		outsideNormalBMI(namesMain, bmiMain);
 
 		// console separation
 		System.out.println("========== MHR Analysis ==========");
 		// calculates average Max Heart Rate from the team
 		averageMHRMain = calculateAverageMHR(heartRatesMain);
 		calculateHighestMHR(namesMain, heartRatesMain);
-		// all athlete that have an mhr above average have their names listed
-		displayAboveAverage(namesMain, heartRatesMain, averageMHRMain);
 		// console separation
 		System.out.print("\n");
-		// asks user if they want to calculate training heart rates (displays them too)
-		displayTrainingHeartRate(namesMain, heartRatesMain, inputMain);
+
 		// console separation
 		System.out.print("\n**************************************\n" + "Training Program Analysis Complete\n"
 				+ "**************************************\n");
@@ -131,53 +120,6 @@ public class ProjectIteration02
 	}
 
 	/**
-	 * Allows the user to input custom data like names, weight, height, and age
-	 * Calculates bmi and mhr using other methods Just sets up the parallel arrays
-	 * 
-	 * @param names      is for setting names
-	 * @param bmi        is determined by weight and height
-	 * @param heartRates determined by age
-	 * @param input      a scanner for the custom inputs
-	 */
-	public static void enterAthleteData(String[] names, double[] bmi, double[] heartRates, Scanner input)
-	{
-		// allows the user to assign each athlete a name, weight, height and age.
-		for (int count = 0; count < names.length; ++count)
-		{
-			System.out.print("Enter athlete's first name: ");
-			names[count] = input.next();
-
-			double weight = 0;
-			double height = 0;
-			int age = 0;
-
-			do
-			{
-				System.out.print("Enter weight in pounds: ");
-				weight = input.nextDouble();
-			} while (getValidNumber(weight) == false);
-
-			do
-			{
-				System.out.print("Enter height in inches: ");
-				height = input.nextDouble();
-			} while (getValidNumber(height) == false);
-
-			// completes the bmi calculation in the background (no display for calc)
-			bmi[count] = calculateBMI(weight, height);
-			do
-			{
-				System.out.print("Enter age in years: " + "\r");
-				age = input.nextInt();
-
-			} while (getValidNumber(age) == false);
-			// completes the mhr calculation in the background (no display for calc)
-			heartRates[count] = calculateMHR(age);
-
-		} // end of for loop
-	}// end enterAthleteData
-
-	/**
 	 * Calculates BMI using the formula
 	 * 
 	 * @param weight inputed in enterAthleteData
@@ -212,27 +154,6 @@ public class ProjectIteration02
 	}
 
 	/**
-	 * Just prints the names bmi and mhr of all athletes.
-	 * 
-	 * @param nameBMI    names of athletes
-	 * @param bmiDisplay bmi for athletes
-	 * @param mhrDisplay max heart rates of athletes
-	 */
-	public static void displayAthleteBMI(String[] nameBMI, double[] bmiDisplay, double[] mhrDisplay)
-	{
-		// basic list of athletes and their newly calculated stats (IE: BMI and MHR)
-		for (int count = 0; count < nameBMI.length; ++count)
-		{
-			System.out.println(nameBMI[count]);
-			System.out.println("BMI: " + bmiDisplay[count]);
-			// category is calculated during the print part so that it isn't taking up space
-			// in memory
-			System.out.println("Category: " + bmiCategory(bmiDisplay[count]));
-			System.out.println("MHR: " + mhrDisplay[count] + "\n");
-		}
-	}
-
-	/**
 	 * Determines BMI category based on BMI ranges
 	 * 
 	 * Category Scale: Obese: > 40 Overweight: 39.999 to 25 Normal: 24.999 to 18.5
@@ -260,44 +181,6 @@ public class ProjectIteration02
 		} // end else
 
 		return bmiCategoryName;
-	}
-
-	/**
-	 * Displays the names and orientation relative to the normal bmi category
-	 * parameter
-	 * 
-	 * Above Normal: > 25 Below Normal: < 18.4999
-	 * 
-	 * if neither apply and they've gone through every Athlete, it displays that all
-	 * are normal
-	 *
-	 * @param nameNoNorm athlete names
-	 * @param bmiNoNorm  athlete Calculated bmi
-	 */
-	public static void outsideNormalBMI(String[] nameNoNorm, double[] bmiNoNorm)
-	{
-		int normalBMI = 0;
-
-		for (int count = 0; count < nameNoNorm.length; ++count)
-		{
-			// different messages depending on if the athlete is above or below normal BMI
-			if (bmiNoNorm[count] >= 25)
-			{
-				System.out.println("Above Normal: " + nameNoNorm[count]);
-			} else if (bmiNoNorm[count] < 18.5)
-			{
-				System.out.println("Below Normal: " + nameNoNorm[count]);
-			} else
-			{
-				++normalBMI;
-			}
-		}
-		// if all of them are within normal this is run
-		if (normalBMI == nameNoNorm.length)
-		{
-			System.out.println("No athletes outside of normal range");
-		}
-		System.out.print("\n");
 	}
 
 	/**
@@ -345,26 +228,18 @@ public class ProjectIteration02
 
 		return averageMHR;
 	}
+}// end class
 
-	/**
-	 * displays the names of everyone that has a max heart rate higher or equal to
-	 * average max heart rate
-	 * 
-	 * @param nameAbove      names of all the athletes on the team
-	 * @param mhrCheckAbove  max heart rates of all the athletes
-	 * @param averageCompare average of all max heart rates
-	 */
-	public static void displayAboveAverage(String[] nameAbove, double[] mhrCheckAbove, double averageCompare)
+class Athletes
+{
+	private String name;
+	private double weight;
+	private double height;
+	private double age;
+
+	Athletes()
 	{
-		System.out.print("Athletes above or equal to average MHR: \n");
-		for (int count = 0; count < nameAbove.length; ++count)
-		{
-			// prints and athletes name if they are above or equal to averageMHR
-			if (mhrCheckAbove[count] >= averageCompare)
-			{
-				System.out.println(nameAbove[count]);
-			}
-		}
+
 	}
 
-}// end class
+}
